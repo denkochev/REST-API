02b34ecd6a9018795f37d314955d85f1ef6131ad
denkochev <denkochev78@gmail.com>
2023-05-08 20:13:36 +0200

    Final commit

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const path = require('path');
+const {v4} = require('uuid');
 const app = express();
 
 
-const CONTACTS = [
-    {id: 1, name: 'Denys', value: '+380-626-82-15', marked: false}
+let CONTACTS = [
+    {id: v4(), name: 'Denys', value: '+380-626-82-15', marked: false}
 ]
+
+app.use(express.json())
 
 app.get('/api/contacts', (req, res) => {
     res.status(200).json(CONTACTS)
-})
+});
+
+app.post('/api/contacts', (req, res) => {
+    const contact = {...req.body, id: v4(), marked: false}
+    CONTACTS.push(contact);
+    res.status(201).json(contact);
+});
+
+app.delete('/api/contacts/:id', (req, res) => {
+    CONTACTS = CONTACTS.filter(c => c.id !== req.params.id);
+    res.status(200).json({message: 'Contact was deleted'});
+});
+
+app.put('/api/contacts/:id', (req, res) => {
+    const idx = CONTACTS.findIndex(c => c.id === req.params.id);
+    CONTACTS[idx] = req.body;
+    res.status(200).json(CONTACTS[idx]);
+});
 
 app.use(express.static(path.resolve(__dirname, '../client')));
 
@@ -17,4 +37,3 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'index.html'));
 });
 app.listen(3000, () => console.log('Server has been started on port 3000...'));
-
